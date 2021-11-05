f2a98d6557516b80d306d60fcd299607d205f475
Jeremy Mi <mizhemin@gmail.com>
2021-11-04 22:44:18 -0700

    Change client to V2

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calcultator for Kubernetes
+            Fib Calcultator for Kubernetes V2!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
